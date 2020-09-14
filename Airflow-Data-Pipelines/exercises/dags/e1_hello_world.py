5b48685c3ee625a0b4283b9f7f1044f7df213a41
naderasadi <asadi.nader97@gmail.com>
2020-09-14 14:33:13 +0430

    Airflow module one exercises completed

---
+++ Airflow-Data-Pipelines/exercises/dags/e1_hello_world.py
@@ -0,0 +1,16 @@
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def hello_world():
+    logging.info('Hello World!')
+
+dag = DAG(dag_id='lesson1.exercise1', start_date=datetime.datetime.now())
+
+greet_task = PythonOperator(
+    task_id = 'hello_world_task',
+    python_callable = hello_world,
+    dag = dag
+)
